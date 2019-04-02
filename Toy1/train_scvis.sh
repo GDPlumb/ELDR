9e9f29f97962d7a9e6e560f4b2085bfb6157bef1
GDPlumb <plumbdg@gmail.com>
2019-04-02 15:15:30 -0400

    Switched to 9d synethetic, cleaned up structure, and added visualization of the explanation

--- Toy1/train_scvis.sh
+++
@@ -1,5 +0,0 @@
-# TODO:  use 9d data
-
-cd ../scvis-dev
-scvis train --data_matrix_file ./data/synthetic_2d_2200.tsv --out_dir ./output/ --data_label_file ./data/synthetic_2d_2200_label.tsv --verbose --verbose_interval 50
-mv output ../Toy1/output
