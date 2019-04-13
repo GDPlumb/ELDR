c113a7d3fe42d4f636cba08ac246f7f4f8762c1b
GDPlumb <plumbdg@gmail.com>
2019-04-13 18:56:51 -0400

    trained bipolar model on pca data

---
+++ scivis/bipolar/train.sh
@@ -0,0 +1,3 @@
+
+cd ../scvis-dev
+python scvis train --data_matrix_file ./data/bipolar_pca100.tsv --out_dir ../bipolar/model/ --data_label_file ./data/bipolar_label.tsv --verbose --verbose_interval 50
