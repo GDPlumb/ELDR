b5d5c11982e57966cb7c0e2704dd7b22235e714e
GDPlumb <plumbdg@gmail.com>
2020-02-03 15:43:15 -0500

    Updated the sparsity-metrics trade-off experiments

--- Code/misc.py
+++ Code/misc.py
@@ -23,3 +23,26 @@ def truncate(values, k):
     values_aprox = np.zeros(values.shape)
     values_aprox[idx] = values[idx]
     return values_aprox
+    
+def load(deltas, k, initial, target):
+
+    if initial == 0:
+        d = deltas[target - 1]
+    elif target == 0:
+        d = -1.0 * deltas[initial - 1]
+    else:
+        d = -1.0 * deltas[initial - 1] + deltas[target - 1]
+        
+    d = truncate(d, k)
+
+    return d
+
+# e_more should be a sparser vector than e_less
+# counts the percentage of e_more's explanation that is in features chosen by e_less
+def stability(e_more, e_less):
+    difference = 0
+    for i in range(e_more.shape[0]):
+        if e_less[i] != 0:
+            difference += np.abs(e_more[i])
+    return difference / np.sum(np.abs(e_more))
+
